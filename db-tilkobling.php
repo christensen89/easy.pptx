ec76042c28885544203d278cc688da894f2662dc
christensen89 <fredrik.christensen@student.hbv.no>
2016-04-14 10:51:59 +0200

    Delete db-tilkobling.php

--- db-tilkobling.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$host="aulesjord2.mysql.domeneshop.no";
-	$user="aulesjord2";
-	$password="VKcHWVErGPnS2XP";
-	$database="aulesjord2";
-	$db=mysqli_connect($host, $user, $password, $database) or die ("Ikke kontakt med server");
-?>
